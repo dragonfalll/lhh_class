ec580939548321b535c7dddc5fad16c8bc34a71f
kashawn jimerson <kashawnjimerson4@gmail.com>
2018-10-24 09:29:31 -0700

    Work on dealing cards.

--- deal.js
+++ deal.js
@@ -1,9 +1,15 @@
 const columnCount = 7;
 
 function deal() {
-    for (let i = 0; i<7;i++) {
-        let card = this.cards[i];
-
-        this.columns[i].cards.push(card);
+    for (let row = 0; row < 7; row++) {
+        for (let col = row; col < 7; col++) {
+            let card = this.cards[col];
+            this.columns[col].cards.push(card);
+        }
     }
+    /*
+        for (let col = 1; col < 7 ; col++) {
+            let card = this.cards[col];
+            this.columns[col].cards.push(card);
+        }*/
 }
