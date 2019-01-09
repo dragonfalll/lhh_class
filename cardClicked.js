561bcac49ab7fcd6cd07f4bbc07c94ed1ea4cd48
kashawn jimerson <kashawnjimerson4@gmail.com>
2019-01-09 01:14:53 -0800

    Make cards move.

--- cardClicked.js
+++ cardClicked.js
@@ -1,11 +1,16 @@
-function cardClicked(card, column, lastCard, lastClicked) {
+function cardClicked(card, column, lastCard, lastColumn) {
+    if (card.reversed == true ){
+        card.reversed == false
+    }
     if (lastCard == null) {
         card.selected = true;
         this.remember(card, column);
 
     } else {
-        alert(lastCard.name() + 'moving');
+        let cardsToMove = lastColumn.popFrom(lastCard);
+        column.append(cardsToMove);
         lastCard.selected = false;
         this.forget();
+        
     }
 }
